6d350655e52763461a9d33847743d2cf6983e610
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-29 18:44:56 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,10 +2,10 @@
 echo "hello world"
 scp -r ${WORKSPACE} root@10.3.20.206:/var/tmp/
 ssh root@10.3.20.206 "whoami;echo 'build number '+${BUILD_NUMBER};
-                      export BUILD_NUMBER;
+                      export BUILD_NUMBER=$BUILD_NUMBER;
                        echo 'build name '+${BUILD_NAME};
                        echo 'workspace name '+${WORKSPACE};
                        echo 'WA_SRC_FOLDER '+${WA_SRC_FOLDER};
                        sudo chmod 777  /var/tmp/genapp-github/getports-jks1.sh;
                        cd /var/tmp/genapp-github;
-                       source ./getports-jks1.sh"
+                       ./getports-jks1.sh"
