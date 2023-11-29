2262048d86212c23660a97ee7376184a8206a1df
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-29 17:40:18 +0100

    Update getports-jks1.sh

--- getports-jks1.sh
+++ getports-jks1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "shell pid= " $$
 WA_DST_FOLDER=/home/wazi/data/project
+WA_SRC_FOLDER=/var/tmp/genapp-github
 BASE_PORT=29000
 INCREMENT=1
 port1=$BASE_PORT
@@ -21,9 +22,9 @@ sudo podman pull docker.io/wfezzani/wazi-analyze:2023.2.0
 echo "port1 =" $port1
 echo "port2 =" $port2
 sudo podman run -d -it -p $port1:5000 -p $port2:$port2 -e AZN_API_SERVER_PORT=$port2 --name GENAPP-${BUILD_NUMBER} docker.io/wfezzani/wazi-analyze:2023.2.0
-sudo podman cp -a /var/tmp/genapp-github/. GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER/source/
-sudo podman cp /var/tmp/genapp-github/project.dat GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER
-sudo podman cp /var/tmp/genapp-github/wa-script.sh GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER
+sudo podman cp -a $WA_SRC_FOLDER/. GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER/source/
+sudo podman cp $WA_SRC_FOLDER/project.dat GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER
+sudo podman cp $WA_SRC_FOLDER/wa-script.sh GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER
 #sudo podman exec -e AZN_API_SERVER_PORT=$port2 GENAPP-${BUILD_NUMBER} env
 sudo podman exec GENAPP-${BUILD_NUMBER} chmod 777 /home/wazi/data/project/wa-script.sh
 sudo podman exec GENAPP-${BUILD_NUMBER} /home/wazi/data/project/wa-script.sh
