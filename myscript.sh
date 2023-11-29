670d8acc43055d91bbf0305f749cae7c6688b20b
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-29 18:42:34 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,6 +2,7 @@
 echo "hello world"
 scp -r ${WORKSPACE} root@10.3.20.206:/var/tmp/
 ssh root@10.3.20.206 "whoami;echo 'build number '+${BUILD_NUMBER};
+                      export BUILD_NUMBER;
                        echo 'build name '+${BUILD_NAME};
                        echo 'workspace name '+${WORKSPACE};
                        echo 'WA_SRC_FOLDER '+${WA_SRC_FOLDER};
