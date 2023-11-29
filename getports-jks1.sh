f59f0371ae7a7dc1d60950d739cf20a78f854c2a
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-29 17:26:25 +0100

    Create getports-jks1.sh

---
+++ getports-jks1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "shell pid= " $$
+WA_DST_FOLDER=/home/wazi/data/project
+BASE_PORT=29000
+INCREMENT=1
+port1=$BASE_PORT
+isfree=$(netstat -taln | grep $port1)
+while [[ -n "$isfree" ]]; do
+  port1=$[port1+INCREMENT]
+  isfree=$(netstat -taln | grep $port1)
+done
+echo "First Usable Port: $port1"
+port2=$[port1+INCREMENT]
+isfree=$(netstat -taln | grep $port2)
+while [[ -n "$isfree" ]]; do
+  port2=$[port2+INCREMENT]
+  isfree=$(netstat -taln | grep $port2)
+done
+echo "Second Usable Port: $port2"
+sudo podman pull docker.io/wfezzani/wazi-analyze:2023.2.0
+echo "port1 =" $port1
+echo "port2 =" $port2
+sudo podman run -d -it -p $port1:5000 -p $port2:$port2 -e AZN_API_SERVER_PORT=$port2 --name GENAPP-${BUILD_NUMBER} docker.io/wfezzani/wazi-analyze:2023.2.0
+sudo podman cp -a /var/tmp/genapp-github/. GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER/source/
+sudo podman cp /var/tmp/genapp-github/project.dat GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER
+sudo podman cp /var/tmp/genapp-github/wa-script.sh GENAPP-${BUILD_NUMBER}:$WA_DST_FOLDER
+#sudo podman exec -e AZN_API_SERVER_PORT=$port2 GENAPP-${BUILD_NUMBER} env
+sudo podman exec GENAPP-${BUILD_NUMBER} chmod 777 /home/wazi/data/project/wa-script.sh
+sudo podman exec GENAPP-${BUILD_NUMBER} /home/wazi/data/project/wa-script.sh
+echo "l'interface graphique est accessible à l'adresse suivante: https://10.3.20.206:$port1/login?api-server=10.3.20.206:$port2"
