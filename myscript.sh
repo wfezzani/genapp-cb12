1216c617682960f661f7d8cc9a12e93f509aa504
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-29 17:45:56 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -6,4 +6,4 @@ ssh root@10.3.20.206 "whoami;echo 'build number '+${BUILD_NUMBER};
                        echo 'workspace name '+${WORKSPACE};
                        echo 'WA_SRC_FOLDER '+${WA_SRC_FOLDER};
                        sudo chmod 777  /var/tmp/genapp-github/getports-jks1.sh;
-                       /var/tmp/genapp-github/getports-jks1.sh"
+                       source /var/tmp/genapp-github/getports-jks1.sh"
