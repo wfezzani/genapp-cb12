fcb36eea61a5bf0d42a9fc72963dc481b1d1d531
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-30 16:26:29 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -6,6 +6,7 @@ IPADDRESS=10.3.20.206
 GIT_FOLDER=/var/tmp/
 # Tranfer the git repository from the Jenkins server to the Docker server
 scp -r ${WORKSPACE} $USER@$IPADDRESS:$GIT_FOLDER
+# SSH to the Docker host and execute the script
 ssh $USER@$IPADDRESS "export BUILD_NUMBER=$BUILD_NUMBER;
                        sudo chmod 777  $GIT_FOLDER/$JOB_BASE_NAME/getports-jks1.sh;
                        cd $GIT_FOLDER/$JOB_BASE_NAME;
