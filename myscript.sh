de9e800c1d2181e68f7a00dea8e291b62dcc4ef7
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-30 15:44:04 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -4,7 +4,7 @@ USER=root
 IPADDRESS=10.3.20.206
 # Destinantion folder on the Docker host 
 GIT_FOLDER=/var/tmp/
-echo "build name= "+$BUILD_DISPLAY_NAME
+echo "build name= "+$JOB_NAME
 # Tranfer the git repository from the Jenkins server to the Docker server
 scp -r ${WORKSPACE} $USER@$IPADDRESS:$GIT_FOLDER
 ssh $USER@$IPADDRESS "whoami;echo 'build number '+${BUILD_NUMBER};
