ca090a7b1c67b976d655e6aa221351fb9436daa5
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-30 15:42:41 +0100

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# UserID and PASSWORD for the Docker host 
 USER=root
 IPADDRESS=10.3.20.206
-scp -r ${WORKSPACE} $USER@$IPADDRESS:/var/tmp/
+# Destinantion folder on the Docker host 
+GIT_FOLDER=/var/tmp/
+echo "build name= "+$BUILD_DISPLAY_NAME
+# Tranfer the git repository from the Jenkins server to the Docker server
+scp -r ${WORKSPACE} $USER@$IPADDRESS:$GIT_FOLDER
 ssh $USER@$IPADDRESS "whoami;echo 'build number '+${BUILD_NUMBER};
                       export BUILD_NUMBER=$BUILD_NUMBER;
-                       echo 'build name '+${BUILD_NAME};
-                       echo 'workspace name '+${WORKSPACE};
-                       echo 'WA_SRC_FOLDER '+${WA_SRC_FOLDER};
-                       sudo chmod 777  /var/tmp/genapp-github/getports-jks1.sh;
+                       sudo chmod 777  $GIT_FOLDER/genapp-github/getports-jks1.sh;
                        cd /var/tmp/genapp-github;
                        ./getports-jks1.sh"
