80ed4f15bb9785af9369e5b7097f1f1ca69a49fc
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-30 17:32:42 +0100

    Update project_process.sh

--- project_process.sh
+++ project_process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wa-scan.sh project /home/wazi/data/project/project.dat
 echo "project scan completed :) "
-wa-startup.sh -p "ztecztec" > startup.log
+wa-startup.sh -p "password" > startup.log
 echo "Wazi Web UI is now available ! "
