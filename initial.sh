6dcb10951dd68c0031f75838bbde7abbb2cc9b65
wfezzani <37804616+wfezzani@users.noreply.github.com>
2023-11-30 17:10:59 +0100

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # UserID and PASSWORD for the Docker host 
-export USER=root
-export IPADDRESS=10.3.20.206
+USER=root
+IPADDRESS=10.3.20.206
 # Destinantion folder on the Docker host 
 GIT_FOLDER=/var/tmp/
 # Tranfer the git repository from the Jenkins server to the Docker server
@@ -9,6 +9,7 @@ scp -r ${WORKSPACE} $USER@$IPADDRESS:$GIT_FOLDER
 # SSH to the Docker host and execute the script
 ssh $USER@$IPADDRESS "export BUILD_NUMBER=$BUILD_NUMBER;
                       export GIT_FOLDER=$GIT_FOLDER;
+                      export IPADDRESS=$IPADDRESS;
                       export JOB_BASE_NAME=$JOB_BASE_NAME;
                        sudo chmod 777  $GIT_FOLDER/$JOB_BASE_NAME/docker_process.sh;
                        cd $GIT_FOLDER/$JOB_BASE_NAME;
